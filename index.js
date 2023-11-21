ffc28714e6190888e7db6eb64b73d579355bd4cd
Folarin Olabinri <[olabinrifolarin@gmail.com])>
2023-11-21 20:22:56 +0100

    reversal clash

--- index.js
+++ index.js
@@ -36,56 +36,56 @@ app.use(
   })
 );
 
-// const scriptSrcUrls = [
-//   "https://stackpath.bootstrapcdn.com/",
-//   "https://kit.fontawesome.com/",
-//   "https://cdnjs.cloudflare.com/",
-//   "https://cdn.jsdelivr.net",
-//   "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
-//   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js",
-//   "https://code.jquery.com/jquery-1.12.0.min.js",
-//   "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-//   "https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js",
-//   "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/js/swiper.min.js",
-//   "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css",
-//   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
-// ];
-// const styleSrcUrls = [
-//   "https://kit-free.fontawesome.com/",
-//   "https://stackpath.bootstrapcdn.com/",
-//   "https://fonts.googleapis.com/",
-//   "https://use.fontawesome.com/",
-//   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.min.css",
-//   "https://fonts.cdnfonts.com/css/effra-heavy",
-//   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.transitions.css",
-//   "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
-//   "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css",
-// ];
-// const connectSrcUrls = [
-//   "https://api.mapbox.com/",
-//   "https://a.tiles.mapbox.com/",
-//   "https://b.tiles.mapbox.com/",
-//   "https://events.mapbox.com/",
-// ];
-// const fontSrcUrls = [
-//   "https://kit-free.fontawesome.com/",
-//   "https://stackpath.bootstrapcdn.com/",
-//   "https://fonts.googleapis.com/",
-//   "https://use.fontawesome.com/",
-//   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.min.css",
-//   "https://fonts.cdnfonts.com/css/effra-heavy",
-//   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css",
-//   "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.transitions.css",
-//   "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
-//   "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css",
-// ];
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js",
+  "https://code.jquery.com/jquery-1.12.0.min.js",
+  "https://code.jquery.com/jquery-3.5.1.slim.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/js/swiper.min.js",
+  "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.min.css",
+  "https://fonts.cdnfonts.com/css/effra-heavy",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.transitions.css",
+  "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+  "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.min.css",
+  "https://fonts.cdnfonts.com/css/effra-heavy",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.transitions.css",
+  "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+  "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css",
+];
 
 app.use(
   helmet.contentSecurityPolicy({
